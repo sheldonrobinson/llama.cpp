3af21b95bb822b5ca9c2a696f974b2fb5052c402
Sheldon Robinson <sheldon.robinson@gmail.com>
2026-02-06 09:14:30 -0500

    Fix linkage errors

--- tools/embedded/server-embedded.h
+++ tools/embedded/server-embedded.h
@@ -55,14 +55,14 @@ extern "C"
  *  └────────unloaded─────────┘
  */
 
-enum server_model_status {
+enum server_model_status : int8_t {
     SERVER_MODEL_STATUS_UNLOADED,
     SERVER_MODEL_STATUS_LOADING,
     SERVER_MODEL_STATUS_LOADED,
 	SERVER_MODEL_STATUS_INVALID
 };
 
-typedef enum server_embedded_status {
+typedef enum server_embedded_status : int8_t {
     // TODO: also add downloading state when the logic is added
     SERVER_EMBEDDED_STATUS_STARTING,
     SERVER_EMBEDDED_STATUS_STARTED,
